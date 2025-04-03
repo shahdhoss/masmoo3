52bd3e53410e42c18d183d58a832ef872e73fb7a
Alhusseain Shalaby <husseainshalaby6@gmail.com>
2025-04-04 00:05:47 +0200

    nav in prog 3

--- web-app/client/src/Components/NavBar.jsx
+++ web-app/client/src/Components/NavBar.jsx
@@ -5,12 +5,13 @@ import download from "./Assets/images/download-arrow.png"
 import options from "./Assets/images/Options.png"
 import Searchkit from "./Assets/NavComp/SearchKit";
 import PlayBack from "./Assets/NavComp/PlayBack";
+import masmoo3 from "./Assets/images/masmoo3.png";
 const NavBar = () => {
     return(
     <div>
         <div className="flex justify-between items-center bg-gray-50 shadow-sm p-4 ps-10">
-            <div className="text-lg font-bold">MyApp</div>
-           <Searchkit/>
+            <a href="/home"><img src={masmoo3} className="size-8"/></a>
+            <Searchkit/>
             <div className="flex justify-center items-center space-x-4">
                 <PlayBack/>
                 <div className="flex justify-between items-center space-x-10 pe-10 shrink-0">
