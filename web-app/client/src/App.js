eb7f8995ea3acfb91b7ac9c4001ec1f37f707117
Alhusseain Shalaby <husseainshalaby6@gmail.com>
2025-04-03 19:29:25 +0200

    nav in prog

--- web-app/client/src/App.js
+++ web-app/client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
+import './Components/Assets/css/tailwind.css';
 import './App.css';
+<<<<<<< Updated upstream
 
 function App() {
   return (
@@ -18,6 +20,24 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegistrationForm from './Components/LoginSignup/RegistrationForm';
+import LoginForm from './Components/LoginSignup/LoginForm';
+import Logintest from './Components/LoginSignup/Logintest';
+import NavBar from './Components/NavBar';
+function App() {
+  return (
+    <div >
+    <nav><NavBar/></nav>
+    <Router>
+      <Routes>
+        <Route path='/signup' element={<RegistrationForm/>}></Route>
+        <Route path='/login' element={<LoginForm/>}></Route>
+        <Route path='/success' element={<Logintest/>}></Route>
+      </Routes>
+    </Router>
+>>>>>>> Stashed changes
     </div>
   );
 }
