c7804b1ef4d8b5f6cedd3ff8fc8e63ebc15b5da9
Alhusseain Shalaby <husseainshalaby6@gmail.com>
2025-04-04 13:25:38 +0200

    navbar in prog 4 - base finished yet needs addition

--- web-app/client/src/Components/NavBar.jsx
+++ web-app/client/src/Components/NavBar.jsx
@@ -9,15 +9,15 @@ import masmoo3 from "./Assets/images/masmoo3.png";
 const NavBar = () => {
     return(
     <div>
-        <div className="flex justify-between items-center bg-gray-50 shadow-sm p-4 ps-10">
-            <a href="/home"><img src={masmoo3} className="size-8"/></a>
+        <div className="flex justify-between items-center bg-gray-50 shadow-sm p-4 ps-10 space-x-3">
+            <div className="shrink-0"><a href="/home"><img src={masmoo3} className="size-8"/></a></div>
             <Searchkit/>
             <div className="flex justify-center items-center space-x-4">
                 <PlayBack/>
-                <div className="flex justify-between items-center space-x-10 pe-10 shrink-0">
-                    <a href="/login"><img src={download} className="size-7"/></a>
-                    <a href="/home"><img  src={profile} className="size-7"/></a>
-                    <a href="/signup"><img  src={options} className="size-7"/></a>
+                <div className="flex justify-between items-center space-x-5 pe-10 shrink-0">
+                    <a href="/login"><img src={download} className="size-5"/></a>
+                    <a href="/home"><img  src={profile} className="size-5"/></a>
+                    <a href="/signup"><img  src={options} className="size-5"/></a>
                 </div>
             </div>
         </div>
