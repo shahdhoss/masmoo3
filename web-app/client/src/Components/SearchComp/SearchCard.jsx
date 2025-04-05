6c69840cc754bccbcb0c9ce18151b318f7bb6944
alhusseain <husseainshalaby6@gmail.com>
2025-04-05 23:37:07 +0200

    filters and I am still believing

--- web-app/client/src/Components/SearchComp/SearchCard.jsx
+++ web-app/client/src/Components/SearchComp/SearchCard.jsx
@@ -7,7 +7,7 @@ const SearchCard = () => {
     const [img, setImg] = useState(imgUrl);
 
     return (
-        <div className="border-2 border-[#00332c] flex justify-center items-center p-4  rounded-lg w-80 h-64 space-x-4 bg-gray-50 shadow-lg">
+        <div className="border-2 border-green-site flex justify-center items-center p-4  rounded-lg w-80 h-64 space-x-4 bg-gray-50 shadow-lg">
             <div className="relative h-56 bg-gray-900 w-32">
                 {/* Bookmark Image */}
                 <img src={bookmark} alt="Bookmark" className="absolute top-0 right-0 w-8 h-8" />
--- web-app/client/src/Components/SearchPage.jsx
+++ web-app/client/src/Components/SearchPage.jsx
@@ -1,17 +1,22 @@
 import "./Assets/css/tailwind.css";
+import FilterBar from "./SearchComp/FilterBar";
 import SearchCard from "./SearchComp/SearchCard";
 
 const SearchPage = () => {
     return (
-        <div className="flex justify-start p-15 space-x-10 flex-wrap space-y-12 ">
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
+        <div className="p-15 space-y-8">
+            <FilterBar/>
+            <div className="flex justify-start space-x-10 flex-wrap space-y-12 ">
+                <SearchCard/>
+                <SearchCard/>
+                <SearchCard/>
+                <SearchCard/>
+                <SearchCard/>
+                <SearchCard/>
+                <SearchCard/>
+            </div>
         </div>
+
     );
 };
 
