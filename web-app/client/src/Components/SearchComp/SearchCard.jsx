58c4025edaaec1f77bf3ebb1ff9770c3e004d594
alhusseain <husseainshalaby6@gmail.com>
2025-04-05 14:11:41 +0200

    card but I begin to believe 3

--- web-app/client/src/Components/SearchComp/SearchCard.jsx
+++ web-app/client/src/Components/SearchComp/SearchCard.jsx
@@ -7,16 +7,19 @@ const SearchCard = () => {
     const [img, setImg] = useState(imgUrl);
 
     return (
-        <div className="border-3 border-[#00332c] flex justify-center items-center p-4  rounded-lg w-80 h-64 space-x-4 bg-gray-50 shadow-md">
-            <div className="relative h-48 bg-gray-900 w-28">
+        <div className="border-2 border-[#00332c] flex justify-center items-center p-4  rounded-lg w-80 h-64 space-x-4 bg-gray-50 shadow-lg">
+            <div className="relative h-56 bg-gray-900 w-32">
                 {/* Bookmark Image */}
                 <img src={bookmark} alt="Bookmark" className="absolute top-0 right-0 w-8 h-8" />
                 {/* Book Cover Image */}
                 <img src={img} alt="Book Cover" className="h-48 object-scale-down" />
             </div>
-            <div className=" flex flex-col justify-center items-start p-4">
+            <div className=" w-1/2 flex flex-col justify-center items-start p-4">
                 <h2 className="text-xl font-semibold mt-2">Book Title</h2>
                 <p className="text-gray-600">Author Name</p>
+                <div className="text-gray-500 mt-2 text-sm overflow-y-scroll max-h-32">
+                    This is a sample description of the book. It provides a brief overview of the content and themes.
+                </div>
             </div>
         </div>
     );
