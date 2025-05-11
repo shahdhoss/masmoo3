5771055738499a1a033b30c18cfd8fa73bef136b
Alhusseain Shalaby <husseainshalaby6@gmail.com>
2025-05-11 20:00:10 +0300

    offline 5

--- web-app/client/public/serviceworker.js
+++ web-app/client/public/serviceworker.js
@@ -30,7 +30,7 @@ self.addEventListener('install', (event) => {
 
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      const filesToCache = ['/','/index.html'];
+      const filesToCache = ['/','/index.html','/bundle.js'];
       return cache.addAll(filesToCache).catch(err => {
         console.error('Failed to cache some files:', err);
       });
